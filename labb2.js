f3168482bbd5400d286b6eb523c20e051a33e603
Christian Karlsson <krille.karlsson@gmail.com>
2017-04-04 07:46:28 +0200

    addded remove row button

--- labb2.js
+++ labb2.js
@@ -1,65 +1,73 @@
-addEventListener('load', function () {
-  document.getElementById('submitButton').addEventListener('click', onSsnSubmit);
-  var table = document.getElementById("myTable");
-  var savedPNS = [];
+addEventListener('load', () => {
+  const table = document.querySelector('table');
+  const form = document.querySelector('form');
 
-  function onSsnSubmit() {
-    var submitText = document.getElementById('submitText').value;
+  table.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON') {
+      table.deleteRow(e.target.parentNode.parentNode.rowIndex);
+    }
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const submitText = document.getElementById('submitText').value;
     if(isValidSSN(submitText)) {
-      savedPNS.push(submitText);
       addRowToTable(submitText);
       document.getElementById('errorMessage').style.visibility = 'collapse';
     } else {
       document.getElementById('errorMessage').style.visibility = 'visible';
     }
-  }
 
-  function addRowToTable(ssn) {
-    var row = table.insertRow(1);
-    var pnCell = row.insertCell(0);
-    var ageCell = row.insertCell(1);
-    pnCell.innerHTML = ssn;
-    ageCell.innerHTML = calculateAge(ssn);
-  }
+    function addRowToTable(ssn) {
+      const row = table.insertRow(1);
+      const pnCell = row.insertCell(0);
+      const ageCell = row.insertCell(1);
+      pnCell.innerHTML = ssn;
+      ageCell.innerHTML = calculateAge(ssn);
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'X';
+      ageCell.appendChild(removeButton);
+    }
 
-  function calculateAge(ssn) {
-    var currentDate = new Date();
-    var dateToCheck = convertToDate(ssn);
-    var yearDifference = currentDate.getFullYear() - dateToCheck.getFullYear();
-    if(currentDate.getMonth() < dateToCheck.getMonth()) {
-      yearDifference--;
-    } else if(currentDate.getMonth() === dateToCheck.getMonth()) {
-      if(currentDate.getDate() < dateToCheck.getDate()) {
+    function calculateAge(ssn) {
+      const currentDate = new Date();
+      const dateToCheck = convertToDate(ssn);
+      let yearDifference = currentDate.getFullYear() - dateToCheck.getFullYear();
+      if(currentDate.getMonth() < dateToCheck.getMonth()) {
         yearDifference--;
+      } else if(currentDate.getMonth() === dateToCheck.getMonth()) {
+        if(currentDate.getDate() < dateToCheck.getDate()) {
+          yearDifference--;
+        }
       }
+      return yearDifference;
     }
-    return yearDifference;
-  }
 
-  function convertToDate(ssn) {
-    var year = ssn.slice(0,4);
-    var month = ssn.slice(4,6)-1;
-    var day = ssn.slice(6,8);
-    var asDate = new Date(year, month, day);
-    return asDate;
-  }
+    function convertToDate(ssn) {
+      const year = ssn.slice(0,4);
+      const month = ssn.slice(4,6)-1;
+      const day = ssn.slice(6,8);
+      const date = new Date(year, month, day);
+      return date;
+    }
 
-  function isValidSSN(ssn) {
-    if(/\d{8}[-]?\d{4}/.test(ssn)) {
-      var stripedSSN = ssn.slice(2)
-      stripedSSN = stripedSSN.replace(/[^0-9]/, '');
-      var sum = 0;
-      var multiplier = 2;
-      for(var i = 0; i < stripedSSN.length-1; i++) {
-        var currentNumber = stripedSSN[i];
-        var calculatedNumber = currentNumber * multiplier
-        sum += (calculatedNumber >= 10) ? calculatedNumber % 10 + 1 : calculatedNumber;
-        multiplier = (multiplier === 2) ? 1 : 2;
+    function isValidSSN(ssn) {
+      if(/\d{8}[-]?\d{4}/.test(ssn)) {
+        let stripedSSN = ssn.slice(2)
+        stripedSSN = stripedSSN.replace(/[^0-9]/, '');
+        let sum = 0;
+        let multiplier = 2;
+        for(let i = 0; i < stripedSSN.length-1; i++) {
+          const currentNumber = stripedSSN[i];
+          const calculatedNumber = currentNumber * multiplier
+          sum += (calculatedNumber >= 10) ? calculatedNumber % 10 + 1 : calculatedNumber;
+          multiplier = (multiplier === 2) ? 1 : 2;
+        }
+        const checkSum = (sum % 10 === 0) ? 0 : 10 - (sum % 10);
+        const userCheckSum = stripedSSN.slice(-1);
+        return checkSum == userCheckSum;
       }
-      var checkSum = (sum % 10 === 10) ? 0 : 10 - (sum % 10);
-      var userCheckSum = stripedSSN.slice(-1);
-      return checkSum == userCheckSum;
+      return false;
     }
-    return false;
-  }
+  });
 });
